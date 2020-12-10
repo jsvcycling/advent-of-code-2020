4ba17e7a556fd4154db97dc75e122372ace453c0
Joshua Vega <jvega@jsvcycling.com>
2020-12-09 19:08:21 -0500

    [Day 09] More Rust-ification.

--- src/bin/day_09.rs
+++ src/bin/day_09.rs
@@ -1,3 +1,4 @@
+use std::cmp::Ordering;
 use std::fs::read_to_string;
 
 fn part1(values: &[u64]) -> u64 {
@@ -13,38 +14,42 @@ fn part1(values: &[u64]) -> u64 {
         return values[idx];
     }
 
-    return 0;
+    0
 }
 
-fn part2(values: &[u64], target: u64) -> u64 {
+fn part2(values: &[u64], target: &u64) -> u64 {
     for start_idx in 0..values.len() {
         let mut min = u64::MAX;
         let mut max = u64::MIN;
         let mut sum = 0;
 
-        for idx in (start_idx + 1)..values.len() {
-            if values[idx] + sum < target {
-                // If the new total is still less than the target, append this
-                // value the list and move onto the next value.
-                min = u64::min(min, values[idx]);
-                max = u64::max(max, values[idx]);
-
-                sum += values[idx];
-                continue;
-            } else if values[idx] + sum > target {
-                // If we're going to pass the target with this value, we know
-                // this sequence won't work so stop and move onto the next one.
-                break;
+        for val in values.iter().skip(start_idx) {
+            match (val + sum).cmp(target) {
+                Ordering::Less => {
+                    // If the new total is still less than the target, append
+                    // this value the list and move onto the next value.
+                    min = u64::min(min, *val);
+                    max = u64::max(max, *val);
+                    sum += *val;
+                    continue;
+                },
+                Ordering::Greater => {
+                    // If we're going to pass the target with this value, we
+                    // know this sequence won't work so stop and move onto the
+                    // next one.
+                    break;
+                },
+                _ => {
+                    // We found the right sequence!
+                    min = u64::min(min, *val);
+                    max = u64::max(max, *val);
+                    return min + max;
+                }
             }
-
-            // We found the right sequence!
-            min = u64::min(min, values[idx]);
-            max = u64::max(max, values[idx]);
-            return min + max;
         }
     }
 
-    return 0;
+    0
 }
 
 pub fn main() {
@@ -54,5 +59,5 @@ pub fn main() {
     let part1 = part1(&values);
 
     println!("Part 1: {}", part1);
-    println!("Part 2: {}", part2(&values, part1));
+    println!("Part 2: {}", part2(&values, &part1));
 }
